883006210db8c3e2a1f4dda9c6387dddbaddd3e1
MostafaGhidanM <mostafaghidannti26nov@gmail.com>
2024-01-16 17:31:51 +0200

    Test rout of /books

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/books','App\Http\Controllers\BookController@index');
 
