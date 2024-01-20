df7584dffc9f2fac4e2a77c286d0acd9174e431f
MostafaGhidanM <mostafaghidannti26nov@gmail.com>
2024-01-20 22:13:07 +0200

    show books in cards

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -6,18 +6,26 @@ Library
 <h1>
     Books
 </h1>
-<hr>
-@foreach ( $books as $book )
-<a href="{{ route('books.show',$book->id) }}">
-<h3>{{ $book->title }} </h3>  
-</a> 
-<p>{{ $book->desc }}</p>
-<div>
-    <a href="{{ route('books.edit',$book->id) }}" class="btn btn-warning">Edit The Book</a>
-    <a href="{{ route('books.delete', $book->id) }}" class="btn btn-danger">Delete The Book</a>
+<div class="row">
+    @foreach ($books as $book)
+        <div class="col-md-4 mb-4">
+                <div class="card d-flex flex-column h-100">
+                <div class="card-body">
+                    <a href="{{ route('books.show', $book->id) }}">
+                        <h3>{{ $book->title }}</h3>
+                    </a>
+                    <p>{{ $book->desc }}</p>
+                    <div class="mb-0">
+                        <div class="justify-content-between">
+                            <a href="{{ route('books.edit', $book->id) }}" class="btn btn-warning">Edit The Book</a>
+                            <a href="{{ route('books.delete', $book->id) }}" class="btn btn-danger">Delete The Book</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
 </div>
-<hr>
-@endforeach
 <div class="form-group mb-3">
     <a class="btn btn-primary" href="{{ route('books.create') }}">Create A Book</a>
 </div>
